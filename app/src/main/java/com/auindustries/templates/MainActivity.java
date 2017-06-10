b81b92eb78820aeab43bc20a8a669a516a382d72
Daniel Au <audaniel94@gmail.com>
2017-06-09 17:21:49 -0700

    Use a Toolbar instead of the native Action Bar.

--- app/src/main/java/com/auindustries/templates/MainActivity.java
+++ app/src/main/java/com/auindustries/templates/MainActivity.java
@@ -2,6 +2,7 @@ package com.auindustries.templates;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the toolbar as the app bar for the activity
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitle(R.string.main_activity_title);
+        setSupportActionBar(myToolbar);
     }
 }
